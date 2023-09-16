d70ee871cb6ca11741d023fb8fdefe7cf1b7da63
Tev-J <tj_delph@yahoo.com>
2023-09-15 20:34:04 -0400

    recovered files

---
+++ models/flashcard.py
@@ -0,0 +1,21 @@
+from .base import Base
+from sqlalchemy import Column, Integer, String, DateTime, func
+
+
+class Flashcard(Base):
+    __tablename__ = "Flashcard"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    question = Column(String, nullable=False, unique=True)
+    answer = Column(String, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, onupdate=func.now())
+
+    def __repr__(self):
+        return (
+            f"question = {self.question}"
+            + f"answer = {self.answer}"
+            + f"\ncreated_at = {self.created_at}, "
+            + f"\nupdated_at = {self.updated_at}, "
+            + " >"
+        )
---
+++ models/user.py
@@ -0,0 +1,21 @@
+from .base import Base
+from sqlalchemy import Column, Integer, String, DateTime, func
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(55), nullable=False, unique=True)
+    password = Column(String(36), nullable=False)
+
+    def __repr__(self):
+        return (
+            f"\n<User "
+            + f"\nid = {self.id}, "
+            + f"\nusername = {self.username}, "
+            + " >"
+        )
+
+
+print("look, no change")
