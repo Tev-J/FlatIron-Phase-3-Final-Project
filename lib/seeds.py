8dc71b615803bc3c27499dcac5996496209d9cca
Tev-J <tj_delph@yahoo.com>
2023-09-16 12:28:19 -0400

    moved seed file

--- lib/seeds.py
+++ lib/seeds.py
@@ -2,6 +2,7 @@
 
 from ..models import Session, User
 from faker import Faker
+
 fake = Faker()
 
 
@@ -12,10 +13,10 @@ users = [
     User(username=fake.user_name(), password=fake.uuid4()),
     User(username=fake.user_name(), password=fake.uuid4()),
     User(username=fake.user_name(), password=fake.uuid4()),
-    
 ]
 session.bulk_save_objects(users)
 
 print(users)
-import ipdb; ipdb.set_trace()
+import ipdb
 
+ipdb.set_trace()
