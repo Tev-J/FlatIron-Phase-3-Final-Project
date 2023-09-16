fe07eb319d3e095e0785ebe827245baf0eacb893
Tev-J <tj_delph@yahoo.com>
2023-09-16 18:18:00 -0400

    create category table

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -2,3 +2,4 @@ from .base import Base
 from .user import User
 from .sessions import Session, engine
 from .flashcard import Flashcard
+from .category import Category
--- lib/seeds.py
+++ lib/seeds.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from models import Session, User, Flashcard, Base, engine
+from models import Session, User, Flashcard, Category, Base, engine
 from faker import Faker
+from more_data import questions_and_answers, networking_fundamentals_topics
 import ipdb
 
 fake = Faker()
@@ -19,7 +20,7 @@ session.query(Flashcard).delete()
 # if engine.dialect.has_table(engine, "flashcard"):
 #     session.query(Flashcard).delete()
 
-
+# generating user data
 users = [
     User(username=fake.user_name(), password=fake.uuid4()),
     User(username=fake.user_name(), password=fake.uuid4()),
@@ -27,23 +28,25 @@ users = [
     User(username=fake.user_name(), password=fake.uuid4()),
 ]
 session.bulk_save_objects(users)
-# print(users)
-
-from more_data import questions_and_answers
 
 
+# testin flashcards
 flashcards = []
 for item in questions_and_answers:
     flashcard_inst = Flashcard(question=item["question"], answer=item["answer"])
     flashcards.append(flashcard_inst)
 
-# print(flashcards)
 
-# ipdb.set_trace()
 session.bulk_save_objects(flashcards)
 
 
-# ipdb.set_trace()
+categories = []
+for topic in networking_fundamentals_topics:
+    category_inst = Category(name=topic)
+    categories.append(category_inst)
+
+ipdb.set_trace()
+
 
 session.commit()
 session.close()
